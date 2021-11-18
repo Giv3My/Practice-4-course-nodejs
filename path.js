174a6f0195cef035dd7ce6517f1a3451eef9d531
Giv3My <bessvlad45@gmail.com>
2021-11-18 18:14:42 +0200

    Initial commit

---
+++ path.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+console.log("Путь к файлу: ", __filename);
+
+console.log("Название файла: ", path.basename(__filename));
+
+console.log("Расширение файла: ", path.extname(__filename));
+
+console.log("Название директории ", path.dirname(__filename));
+
+console.log(path.parse(__filename)); // Возвращает объект, св-ми которого являются елементы пути к файлу
